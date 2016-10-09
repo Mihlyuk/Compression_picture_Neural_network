b5a885d37c24dd9a6f4dc11477ff6395ccb7082a
Mihlyuk <konstantin.mihlyuk@yandex.ru>
2016-10-09 19:21:49 +0300

    init commit

---
+++ app/Block.ts
@@ -0,0 +1,34 @@
+export default class Block {
+
+    width: number;
+    height: number;
+    startPixelX: number;
+    startPixelY: number;
+    buffer: number[][];
+
+    /**
+     * @param {number} startPixelX
+     * @param {number} startPixelY
+     * @param {number} width
+     * @param {number} height
+     */
+    constructor(startPixelX: number, startPixelY: number, width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.startPixelX = startPixelX;
+        this.startPixelY = startPixelY;
+
+        this.buffer = [];
+    }
+
+    /**
+     * @param {number[][]} pixels
+     */
+    add(pixels: number[][]) {
+        this.buffer = pixels;
+    }
+
+    getMatrix() {
+        return this.buffer;
+    }
+}
